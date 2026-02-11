5a1a29251b4144eae44f735b178bffeb29e762c2
David Grelsson <davidgrelsson@gmail.com>
2026-02-11 20:57:32 +0100

    Format C# files

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,48 +1,49 @@
 return {
-    {
-        'williamboman/mason.nvim',
-        config = function()
-            require('mason').setup({
-                registries = {
-                    'github:mason-org/mason-registry',
-                    'github:Crashdummyy/mason-registry',
-                }
-            })
-        end
-    },
-    {
-        'williamboman/mason-lspconfig.nvim',
-        config = function()
-            require('mason-lspconfig').setup({
-                ensure_installed = {
-                    'lua_ls',
-                    'clangd',
-                    'ts_ls',
-                    'cssls',
-                    'html',
-                    'powershell_es'
-                }
-            })
-        end
-    },
-    {
-        'neovim/nvim-lspconfig',
-        config = function()
-            vim.lsp.enable('lua_ls')
-            vim.lsp.enable('clangd')
-            vim.lsp.enable('ts_ls')
-            vim.lsp.enable('cssls')
-            vim.lsp.enable('html')
-            vim.lsp.config('powershell_es', {
-                bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services/"
-            })
-        end
-    },
-    {
-        'seblyng/roslyn.nvim',
-        ---@module 'roslyn.config'
-        ---@type RoslynNvimConfig
-        ft = { 'cs', 'razor' },
-        opts = {}
-    }
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({
+				registries = {
+					"github:mason-org/mason-registry",
+					"github:Crashdummyy/mason-registry",
+				},
+			})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"stylua",
+					"clangd",
+					"ts_ls",
+					"cssls",
+					"html",
+					"powershell_es",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			vim.lsp.enable("lua_ls")
+			vim.lsp.enable("clangd")
+			vim.lsp.enable("ts_ls")
+			vim.lsp.enable("cssls")
+			vim.lsp.enable("html")
+			vim.lsp.config("powershell_es", {
+				bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services/",
+			})
+		end,
+	},
+	{
+		"seblyng/roslyn.nvim",
+		---@module 'roslyn.config'
+		---@type RoslynNvimConfig
+		ft = { "cs", "razor" },
+		opts = {},
+	},
 }
